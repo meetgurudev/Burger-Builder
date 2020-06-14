5dcbf18acbcbdd6e669c6354ff57f662544db1fe
Yalagala <gurudev96@gmail.com>
2020-06-15 01:45:04 +0530

    Added Logo

--- react-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ react-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../../UI/Logo/Logo';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo />
         <nav>
             <ul></ul>
         </nav>
