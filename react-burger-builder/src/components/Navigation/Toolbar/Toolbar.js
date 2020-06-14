80c1dbc0080a14a212d7df3136fdcd7b8b2cb5e3
Yalagala <gurudev96@gmail.com>
2020-06-15 01:36:50 +0530

    Added Toolbar UI

---
+++ react-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <div>Logo</div>
+        <nav>
+            <ul></ul>
+        </nav>
+    </header>
+);
+
+export default toolbar;
