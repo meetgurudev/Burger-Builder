9e389c1d6854e18326b78dfe8490ef3f384ba3d6
Yalagala <gurudev96@gmail.com>
2020-06-24 23:25:25 +0530

    Added router dom and Route tags

--- react-burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ react-burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,18 +8,16 @@ const CheckoutSummary = (props) => {
 
     return (
         <div className={classes.CheckoutSummary}>
+
             <h1>We hope it tastes well!</h1>
-            <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
+
+            <div style={{ width: '100%', margin: 'auto' }}>
                 <Burger ingredients={props.ingredients} />
             </div>
-            <Button
-                btnType="Danger"
-                clicked
-            >CANCEL</Button>
-            <Button
-                btnType="Success"
-                clicked
-            >CONTINUE</Button>
+
+            <Button btnTypes="Danger" clicked>CANCEL</Button>
+
+            <Button btnTypes="Success" clicked>CONTINUE</Button>
         </div >
     )
 
